88b3867c4f62762eb60e543aacbeb7a5f5138201
Mesh <442103118@student.ksu.edu.sa>
2023-06-28 06:45:49 +0300

    Finally

--- script.js
+++ script.js
@@ -1,66 +1,101 @@
-var board = ['','','','','','','','','']
 
-    var game = document.getElementsByClassName("col-xs-4 box")[2]
-    $(X).appendTo($(".bos")[2])
-    var col = $('#n3')
-    var X = '<i class="fa-solid fa-x" style="color: #005eff;"></i>'
-    var O = '<i class="fa-solid fa-o" style="color: #ff470a;"></i>'
-    var player1 = X
-    var player2 = O
-    var currentPlayer = true;
-    var turn = 4
-    function ava(mark){
-        if(board[4] != '')
-            return false
-        return true
+
+    
+var X = '<i class="fa-solid fa-x" style="color: #005eff;"></i>'
+var O = '<i class="fa-solid fa-o" style="color: #ff470a;"></i>'
+var board = ['','','','','','','','','']
+var isXplay = true;
+
+    
+        $(".box").click(function(){
+    if($(this).html() === "")
+    if(!checkdraw() && !checkWin("X") && !checkWin("O")){
+        if(isXplay){
+            $(this).append(X);
+            var position = $(this).data("x");
+            chosePos(position,"X");
+            nextTurn()
+            if(checkWin("X")){
+                $("h1").html(X+" win");
+                $("body").addClass("Xwin");
+            }
+            if(checkdraw())
+            $("h1").text("DRAW");
+        }else{
+            $(this).append(O);
+            var position = $(this).data("x");
+            chosePos(position,"O");
+            nextTurn()
+            if(checkWin("O")){
+                $("h1").html(O+" win");
+                $("body").addClass("Owin");
+            }
+            
+            if(checkdraw())
+            $("h1").text("DRAW");
+        }
+    }
+        
+  });
+
+
+  function checkdraw(){
+    var draw = true;
+    for(var i = 0; i < board.length ; i++){
+        if(board[i] === "")
+        draw = false;
     }
 
-    function chose(event,Pos){
-        var id = "g"+Pos;
-        
-        $("#n3").html(Pos)
-        var el = $(id)
-        $(player1).appendTo(el)
-        if(currentPlayer){
-            var id = "#n"+Pos
-            var el = $(id)
-            $(player1).appendTo(el)
-            currentPlayer = false
-        }
-        else{
-            var id = "#n"+Pos
-            var el = $(id)
-            $(player2).appendTo(el)
-            currentPlayer = true
-        }
+    return draw;
+  }
 
+  function nextTurn(){
+    if(isXplay){
+        isXplay = false;
+        $("h1").html(O+" Turn")
+    }else{
+        isXplay = true;
+        $("h1").html(X+" Turn")
     }
     
-    function check(Pos){
-        for(var i ; i< $(".box").length;i++){
-            if(Pos == $(".box")[i]){
-                return false
-            }
+    
+  }
+  function chosePos(pos,player){
+    board[pos] = player;
+  }
 
-        }
-        return true
-
+  function checkWin(player){
+    //v
+    if(board[0] === board[1] && board[1] === board[2] && board[2] === player){
+        return true;
     }
-    $(".box").click(function(event){
-        
-        if(check($(".box")[2]))
-        $(X).appendTo($(".box")[2])
-        
-        if(currentPlayer){
-            var domElement = $( this ).get( 0 );
-            $(player1).appendTo(domElement)
-            currentPlayer = false
-        }else{
-            var domElement = $( this ).get( 0 );
-            $(player2).appendTo(domElement)
-            currentPlayer = true
+    if(board[3] === board[4] && board[4] === board[5] && board[5] === player){
+        return true;
+    }
+    if(board[6] === board[7] && board[7] === board[8] && board[8] == player){
+        return true;
+    }
 
-        }
-                
+    //h
+    if(board[0] === board[3] && board[3] === board[6] && board[6] == player){
+        return true;
+    }
+    if(board[1] === board[4] && board[4] === board[7] && board[7] == player){
+        return true;
+    }
+    if(board[2] === board[5] && board[5] === board[8] && board[8] == player){
+        return true;
+    }
 
-  })
+
+    if(board[0] === board[4] && board[4] === board[8] && board[8] == player){
+        return true;
+    }
+    if(board[2] === board[4] && board[4] === board[6] && board[6] == player){
+        return true;
+    }
+    return false;
+
+
+
+  }
